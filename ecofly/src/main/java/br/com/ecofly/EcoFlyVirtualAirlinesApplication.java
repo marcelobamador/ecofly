bd3f14033c0c649e985ccec58cd382c92ef452fb
marce <marce@DESKTOP-FHAVSHJ>
2022-07-02 14:55:47 -0300

    First Commit

---
+++ ecofly/src/main/java/br/com/ecofly/EcoFlyVirtualAirlinesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecofly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFlyVirtualAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoFlyVirtualAirlinesApplication.class, args);
+	}
+
+}
