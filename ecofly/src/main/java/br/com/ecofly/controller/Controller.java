b7852afcc2e4e23579618e3e433b122fa5e010fd
marce <marce@DESKTOP-FHAVSHJ>
2022-07-05 13:21:21 -0300

    Adicionando pagina de login e configuracao da base de dados

--- ecofly/src/main/java/br/com/ecofly/controller/Controller.java
+++ ecofly/src/main/java/br/com/ecofly/controller/Controller.java
@@ -4,8 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-	@RequestMapping(value = "/index")
-	   public String index() {
-	      return "index";
-	   }
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
 }
